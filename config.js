62c27298f290a4d06aed186101b3d6dc5ca44129
musganz01 <96802092+musganz@users.noreply.github.com>
2022-11-14 07:03:52 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 global.you = ['6285850539404'] 
 //Developer Bot
 global.gc = 'https://chat.whatsapp.com/I5Kz3Abawc5DqyBSDoYvKB'
-global.rowner = ['6285850539404'] // Real Owner Bot
-global.owner = ['6285850539404'] // Owner Bot
-global.police = ['6285850539404'] // Police Bot
-global.mods = ['6285850539404']  // Want some help?
-global.prems = ['6285850539404'] // Premium user has unlimited limit
+global.rowner = ['6281235373823'] // Real Owner Bot
+global.owner = ['6281235373823'] // Owner Bot
+global.police = ['6281235373823'] // Police Bot
+global.mods = ['6281235373823']  // Want some help?
+global.prems = ['6281235373823'] // Premium user has unlimited limit
 global.fotonya1 = 'https://telegra.ph/file/00ca947f49354f6c108da.jpg' //ini kalo mau di ganti silahkan ganti aja
 global.fotonya2 = 'https://telegra.ph/file/00ca947f49354f6c108da.jpg' //gw lupa nih foto apa aja:v
 global.fotonya3 = 'https://telegra.ph/file/00ca947f49354f6c108da.jpg' //ini juga ganti
